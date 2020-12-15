d3b6ff283d77c1e0ed660a94a988c009dbc7a29c
gp22 <gp22@tutanota.com>
2020-12-14 19:13:52 -0600

    add static content

--- front-page.php
+++ front-page.php
@@ -1,11 +1,7 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * The template for displaying the front page.
+ * It is based on the generatepress page.php file.
  *
  * @package GeneratePress
  */
@@ -26,52 +22,42 @@ get_header(); ?>
 			 */
 			do_action( 'generate_before_main_content' );
 
-			// if ( generate_has_default_loop() ) {
-			// 	while ( have_posts() ) :
-
-			// 		the_post();
-
-			// 		generate_do_template_part( 'page' );
-
-			// 	endwhile;
-			// }
-
 			?>
+
+			<header class="inside-page-header my-0 py-12 md:py-20 lg:py-28">
+
+				<div class="md:max-w-xl">
+					<h1 class="">Does your website create solutions and peace of mind — or pain and frustration?</h1>
+					<p>👋
+						<span class="text-opacity-75">
+							Hi, I'm Paul. I can help you build trust, and achieve your business goals with exceptional website design and strategy.
+						</span>
+					</p>
+					<a href="" class="font-bold tracking-wider py-3 px-7 text-white uppercase bg-blue-500 inline-block">Get In Touch!</a>
+				</div>
+
+			</header>
 
 			<article class="inside-article">
 
-				<header>
+				<section class="flex flex-wrap md:items-end">
 
-					<div>
-						<h1>Does your website create solutions and peace of mind — or pain and frustration?</h1>
-						<p>👋
-							<span class="text-opacity-75">
-								Hi, I'm Paul. I can help you build trust, and achieve your business goals with exceptional website design and strategy.
-							</span>
-						</p>
-						<button class="btn btn-primary">Get In Touch!</button>
+					<div class="md:w-8/12">
+						<h2>Lorem Ipsum</h2>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 					</div>
 
-				</header>
-
-				<section>
-
-					<div>
-
-						<h2>Lorem Ipsum</h2>
-
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-
+					<div class="flex justify-end md:w-4/12">
+						<img class="transform -rotate-6 border-solid border-8 border-white shadow" src="https://via.placeholder.com/150" alt="">
 					</div>
 
 				</section>
 
 				<section>
 
-				<div>
+					<div>
 
 						<h2>Lorem Ipsum</h2>
-
 						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 
 					</div>
