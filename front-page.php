7ca0b2306546c004e950325491bafb84b80e436a
gp22 <gp22@tutanota.com>
2020-12-28 17:25:19 -0600

    add feature section

--- front-page.php
+++ front-page.php
@@ -50,13 +50,59 @@ get_header(); ?>
 					<?php $about_section = get_field('about_section');
 					if( $about_section ):	?>
 
-						<div class="md:w-8/12">
+						<div class="md:w-7/12">
 							<h2 class="text-4xl"><?php echo $about_section['heading']; ?></h2>
-							<p><?php echo $about_section['copy']; ?></p>
+							<?php echo $about_section['copy']; ?>
 						</div>
 
-						<div class="flex justify-end md:w-4/12">
-							<img class="headshot transform -rotate-6 border-solid border-8 border-white shadow" src="<?php echo esc_url($about_section['headshot']['url']); ?>" alt="<?php echo esc_attr($about_section['headshot']['alt']); ?>"/>
+						<div class="flex justify-end mt-6 transform -rotate-6 md:mt-0 md:w-5/12">
+							<img class="headshot border-solid border-8 border-white shadow" src="<?php echo esc_url($about_section['headshot']['url']); ?>" alt="<?php echo esc_attr($about_section['headshot']['alt']); ?>"/>
+						</div>
+
+					<?php endif; ?>
+
+				</section>
+
+				<section class="mt-24 lg:mt-36">
+
+					<?php $feature_section = get_field('feature_section');
+					if( $feature_section ):	?>
+
+						<div class="flex flex-wrap items-center">
+
+							<div class="pr-12 hidden invisible md:block md:visible md:w-1/2">
+								<img src="<?php echo esc_url($feature_section['feature_image']['url']); ?>" alt="<?php echo esc_attr($feature_section['feature_image']['alt']); ?>">
+							</div>
+
+							<div class="md:w-1/2">
+
+								<div>
+									<h3 class="mb-2 text-base">
+										Strategy & Collaboration
+									</h3>
+									<p class="mb-10 text-sm opacity-75">Your project will be broken into distinct phases with plenty of positive collaboration along the way. You’ll be an active participant in the process!</p>
+								</div>
+								<div>
+									<h3 class="mb-2 text-base">
+										Training & Documentation
+									</h3>
+									<p class="mb-10 text-sm opacity-75">Before your website is finished you’ll get a personal training session to learn how to use it. You’ll get comfortable navigating it, adding content, and maintaining it.</p>
+								</div>
+								<div>
+									<h3 class="mb-2 text-base">
+										Design & Development
+									</h3>
+									<p class="mb-10 text-sm opacity-75">Your website will look modern and make an impression. And, as a developer, I put my years of experience into writing clean code that's easy to maintain, no matter who works on it in the future.</p>
+								</div>
+								<div>
+									<h3 class="mb-2 text-base">
+										Mobile Friendly
+									</h3>
+									<p class="mb-0 text-sm opacity-75">Your website will work and look great on every device. Your visitors will get what they expect whether they’re visiting on a phone, laptop or a large desktop monitor.</p>
+								</div>
+
+							</div>
+
 						</div>
 
 					<?php endif; ?>
