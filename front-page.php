991485cfe29633c48137cbb8940a73a049b05cd0
gp22 <gp22@tutanota.com>
2021-01-02 16:12:53 -0600

    add emojis, update margins

--- front-page.php
+++ front-page.php
@@ -87,25 +87,25 @@ get_header(); ?>
 
 								<li>
 									<h3 class="mb-2 text-base">
-										Strategy & Collaboration
+										📊 Strategy & Collaboration
 									</h3>
 									<p class="mb-10 text-sm opacity-75">Your project will be broken into distinct phases with plenty of positive collaboration along the way. You’ll be an active participant in the process!</p>
 								</li>
 								<li>
 									<h3 class="mb-2 text-base">
-										Training & Documentation
+										🎓 Training & Documentation
 									</h3>
 									<p class="mb-10 text-sm opacity-75">Before your website is finished you’ll get a personal training session to learn how to use it. You’ll get comfortable navigating it, adding content, and maintaining it.</p>
 								</li>
 								<li>
 									<h3 class="mb-2 text-base">
-										Design & Development
+										🎨 Design & Development
 									</h3>
 									<p class="mb-10 text-sm opacity-75">Your website will look modern and make an impression. And, as a developer, I put my years of experience into writing clean code that's easy to maintain, no matter who works on it in the future.</p>
 								</li>
 								<li>
 									<h3 class="mb-2 text-base">
-										Mobile Friendly
+										📱 Mobile Friendly
 									</h3>
 									<p class="mb-0 text-sm opacity-75">Your website will work and look great on every device. Your visitors will get what they expect whether they’re visiting on a phone, laptop or a large desktop monitor.</p>
 								</li>
@@ -140,7 +140,7 @@ get_header(); ?>
 
 				<?php endif; ?>
 
-				<section class="my-24 md:text-center md:my-36">
+				<section class="mt-24 mb-16 md:text-center md:mt-36">
 
 					<h2 class="text-2xl">Ready to Chat?</h2>
 
